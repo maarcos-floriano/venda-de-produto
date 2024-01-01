97b476d8e71c51ba07befefce46352f2221da065
maarcos-floriano <marcos.floriano@sptech.school>
2024-01-01 13:24:19 -0300

    criei a funcionalidade para mudar o fundo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
-function alterarFundo(){
+function alterarFundo(foto){
+    const produto = document.querySelector('.container');
 
+    produto.style.backgroundImage = `url(../assets/${foto}.png)`;
 }
